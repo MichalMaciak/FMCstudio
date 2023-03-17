f07eee7be2557e4cdccc4539a24936a144a1530a
Michal <michal.maciak22@gmail.com>
2023-03-17 16:19:09 +0100

    0.1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,10 @@
 	import Cennik from '$lib/Cennik.svelte';
 	import TvObject from '$lib/3d/TV/TvObject.svelte';
 	import NaszeUslugi from '$lib/NaszeUslugi.svelte';
+  import { useProgress } from '@threlte/extras'
+	import { tweened } from 'svelte/motion'
+  import { fade } from 'svelte/transition'
+
     
     function scrollToDiv() {
     const section = document.getElementById('text');
@@ -28,6 +32,14 @@
     sectionContact.scrollIntoView({ behavior: 'smooth' });
     }
     }
+
+    const { progress } = useProgress()
+
+    const tweenedProgress = tweened($progress, {
+      duration: 800
+    })
+    $: tweenedProgress.set($progress)
+
 </script>
 
 <SvelteSeo
@@ -52,6 +64,21 @@
 
   }}
 />
+
+
+{#if $tweenedProgress < 1}
+	<div
+		transition:fade|local={{
+			duration: 200
+		}}
+		class="wrapper"
+	>
+		<p class="loading">Ładowanie</p>
+		<div class="bar-wrapper">
+			<div class="bar" style="width: {$tweenedProgress * 100}%" />
+		</div>
+	</div>
+{/if}
 
 <main class="bg-neutral-800 w-full">
 
@@ -112,3 +139,36 @@
 
 </main>
 
+<style>
+	.wrapper {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background-color: rgb(38 38 38);
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		align-items: center;
+		justify-content: center;
+		color: white;
+	}
+
+	.loading {
+		font-size: 0.875rem;
+		line-height: 1.25rem;
+	}
+
+	.bar-wrapper {
+		width: 33.333333%;
+		height: 10px;
+		border: 1px solid white;
+		position: relative;
+	}
+
+	.bar {
+		height: 100%;
+		background-color: white;
+	}
+</style>
