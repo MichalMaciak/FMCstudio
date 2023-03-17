583000a9dec85d5c3dd2c387b4214871b31f9003
Michal <michal.maciak22@gmail.com>
2023-03-17 13:41:54 +0100

    0.0.1

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,11 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
 export default  {
+  preprocess:[vitePreprocess({preserve: ['ld+json']}), preprocessThrelte()],
 	kit: {
 		adapter: adapter({
       edge: false,
       split: false
     })
-	},
-	preprocess:[vitePreprocess({preserve: ['ld+json']}), preprocessThrelte()]
-	
+	}
 }
