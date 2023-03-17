aa2467236d0957171b54f997f4d6534dfd8e1adc
Michal <michal.maciak22@gmail.com>
2023-03-17 13:25:22 +0100

    0.1.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
 import adapter from '@sveltejs/adapter-netlify';
+import { preprocessThrelte } from '@threlte/preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
-};
+
+export default  {
+	kit: {
+		adapter: adapter({
+      edge: false,
+      split: false
+    })
+	},
+	preprocess:[vitePreprocess({preserve: ['ld+json']}), preprocessThrelte()]
+	
+}
