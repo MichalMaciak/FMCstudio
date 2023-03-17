dcc17ccba22edb72357596286c93ebf0a0dbd0a2
Michal <michal.maciak22@gmail.com>
2023-03-17 16:04:45 +0100

    0.1

--- src/lib/Cennik.svelte
+++ src/lib/Cennik.svelte
@@ -3,16 +3,17 @@
     <h3 id="2" class="text-5xl text-sky-300 pb-8 font-bold">Cennik 
     </h3>
     <p id="cennik">Każdy projekt jest inny, do wyceny podchodzimy więc indywidualnie. Wynajem studia z obsługą operatora i technika to koszt <span class="text-red-500 font-bold">500 zł </span> netto za godzinę. Koszt wszystkich innych usług poznasz, pisząc na adres </p>
-        <p class="text-center pt-4 leading-relaxed"> <span class="text-red-500 text-3xl ">
+        <p class="text-center pt-4 leading-relaxed"> <span class="text-red-500 text-2xl md:text-3xl ">
             <a href="mailto:katarzyna.mazur@fmcstudio.pl">katarzyna.mazur@fmcstudio.pl</a>
             </span> <br>
-            lub dzwoniąc pod numer<br><span class="text-red-500 text-3xl">
+            lub dzwoniąc pod numer<br><span class="text-red-500 text-2xl md:text-3xl">
                 <a href="tel:+48518334413">+48 518 334 413</a>
                 </span>
         </p>
     <ul>
-        <li class="text-xl">Do kosztu wynajęcia studia w godzinach 23:00 - 6:00 doliczamy <span class="text-red-500 font-bold">30%</span> ceny</li>
-        <li class="text-xl">Do kosztu wynajęcia studia w niedziele i święta doliczamy <span class="text-red-500 font-bold">50%</span> ceny</li>
+        <li>Do kosztu wynajęcia studia w godzinach 23:00 - 6:00 doliczamy <span class="text-red-500 font-bold">30%</span> ceny</li>
+        <li>Do kosztu wynajęcia studia w niedziele i święta doliczamy <span class="text-red-500 font-bold">50%</span> ceny</li>
     </ul>
+    <p class="pb-12 text-center"><span class="font-bold">Adres:<br></span> Domaniewska 37/23 02-672 Warszawa, <br>województwo mazowieckie, Polska. </p>
 </section>
 </div>
--- src/lib/RightImage.svelte
+++ src/lib/RightImage.svelte
@@ -4,14 +4,13 @@
   import studioM2 from '$lib/img/m2.jpg?run'
   import studioS1 from '$lib/img/s1.jpg?run'
   import studioS2 from '$lib/img/s2.jpg?run'
+  import car from '$lib/img/car.jpg?run'
   import Img from '@zerodevx/svelte-img'
 
 </script>
 
-<div class="reletive w-full">
-
-</div>
-<Img class="sticky top-0 w-full object-cover" src={studioM1} alt="FMC Studio zdjecie" />
-<Img class="sticky top-0 w-full object-cover" src={studioM2} alt="FMC Studio zdjecie" />
-<Img class="sticky top-0 w-full object-cover" src={studioS1} alt="FMC Studio zdjecie" />
-<Img class="sticky top-0 w-full object-cover" src={studioS2} alt="FMC Studio zdjecie" />
+<Img class="sticky top-0 w-full object-cover" src={studioM1} alt="FMC Studio małe zdjecie" />
+<Img class="sticky top-0 w-full object-cover" src={studioM2} alt="FMC Studio małe zdjecie" />
+<Img class="sticky top-0 w-full object-cover" src={studioS1} alt="FMC Studio średnie zdjecie" />
+<Img class="sticky top-0 w-full object-cover" src={studioS2} alt="FMC Studio średnie zdjecie" />
+<Img class="sticky top-0 w-full object-cover" src={car} alt="FMC Studio samochód zdjecie" />
